c69304e8574f7d2da22ccb35f4599a551ccc0def
yuhei <yuhei39.apollo@gmail.com>
2019-02-13 22:43:57 +0900

    add bootstrap

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>ぐるなびAPI</title>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-</head>
-<body>
-    {{--<input type="number" id="key" placeholder="検索半径（１〜５）"><input type="button" id="submit" value="送信">--}}
-    {{--@for($i=0;$i<$array.count;$i++)--}}
-    <p> {{$array[0]->name}} </p>
-    <table id="table" border="1">
-        <tr><th>店舗名</th><th>アクセス</th></tr>
-    </table>
-    <a href="{{url('/')}}">戻る</a>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title','一覧')
+@include('layouts.head')
+@include('layouts.header')
+@section('content')
+<p> {{$array[0]->name}} </p>
+<table id="table" border="1">
+    <tr><th>店舗名</th><th>アクセス</th></tr>
+</table>
+<a href="{{url('/search')}}">戻る</a>
+@endsection
+@include('layouts.footer')
--- resources/views/restaurant/show.blade.php
+++ resources/views/restaurant/show.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>ぐるなびAPI</title>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('title','詳細')
+@include('layouts.head')
+@include('layouts.header')
+@section('content')
 <input type="button" id="submit" value="詳細を表示">
 <table id="table" border="1">
     <tr><th>店舗名</th><th>アクセス</th><th>電話番号</th><th>営業時間</th><th>画像</th></tr>
@@ -13,5 +11,5 @@
 <script>
 </script>
     <a href="{{url('/restaurant')}}">戻る</a>
-</body>
-</html>
+@endsection
+@include('layouts.footer')
